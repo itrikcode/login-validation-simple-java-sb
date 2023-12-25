a603876abce7b21cbe6153f0171dab5c86aee108
itrikcode <kumarviveklohaman@gmail.com>
2023-12-25 15:58:52 +0530

    project stracture commit

---
+++ src/main/java/com/itsp/loginvalidationappsb/LoginValidationAppSbApplication.java
@@ -0,0 +1,13 @@
+package com.itsp.loginvalidationappsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginValidationAppSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginValidationAppSbApplication.class, args);
+	}
+
+}
